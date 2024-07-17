2a39b1c8d4eb253388f84150bebce87864c054f5
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-07-17 14:13:37 -0500

    feat: Update app component template and routing configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: '<router-outlet />',
+  // cuando el template es muy simple, se puede usar la propiedad template en lugar de templateUrl y pasarle el router-outlet, tambien se pueden eliminar los archivos app.component.html y app.component.css
 })
 export class AppComponent {
   title = 'angular-store';
