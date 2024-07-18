eddde1be03958966d9a5c93690b2aca102010ec4
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-07-18 16:41:02 -0500

    feat: Add counter component

---
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  @Input({ required: true }) duration = 0; // el motor de TypeScript infiere el tipo de dato de forma implícita
+  @Input({ required: true }) message: string = '';
+
+  // el constructor se usa para crear valores por defecto por lo que no debe ser async
+  constructor() {
+    console.log('Constructor');
+    console.log('-'.repeat(10));
+  }
+}
