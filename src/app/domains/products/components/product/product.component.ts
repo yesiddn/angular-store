0b86bb026b80522e4fd9a3b4dd674f2cf4001172
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-07-18 14:47:52 -0500

    chore: Update product component to add @Inputs

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-  img = `https://picsum.photos/640/640?r=${Math.random()}`;
+  // img = `https://picsum.photos/640/640?r=${Math.random()}`;
+  // Para tener inputs, es decir, propiedades que se pueden pasar desde el padre al hijo se hace con el decorador @Input()
+  @Input({required: true}) img: string = ''; // Se inicializa con un valor por defecto y se pide que sea requerido
+  @Input({required: true}) title: string = '';
+  @Input({required: true}) price: number = 0;
 }
