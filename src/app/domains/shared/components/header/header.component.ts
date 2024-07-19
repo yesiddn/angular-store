fdf48129e81773d319f110946bcfca8594693e5b
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-07-19 15:53:56 -0500

    Update logic to fix total calculation issue

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, Input, signal, SimpleChanges } from '@angular/core';
 import { Product } from '../../models/product.mode';
 import { CommonModule } from '@angular/common';
 
@@ -12,11 +12,20 @@ import { CommonModule } from '@angular/common';
 export class HeaderComponent {
   hideSideMenu = signal(true);
   @Input({ required: true }) cart: Product[] = [];
+  total = signal(0);
 
   toogleSideMenu() {
     this.hideSideMenu.update(prevState => !prevState);
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    const cart = changes['cart'];
+    if (cart) {
+      this.total.set(this.getTotal());
+    }
+  }
+
+  // Llamar a funciones directamente desde el template en Angular puede ser una mala práctica, si esas funciones realizan cálculos o procesamientos pesados y son llamadas frecuentemente, como cada vez que se ejecuta la detección de cambios de Angular. Esto puede llevar a problemas de rendimiento, especialmente si la función se encuentra en una parte del template que se actualiza con mucha frecuencia.
   getTotal() {
     return this.cart.reduce((acc, product) => acc + product.price, 0);
   }
