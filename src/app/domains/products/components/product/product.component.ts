326ad7b9e488d9923994c9bf23f2db173a499714
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-07-19 14:35:35 -0500

    Update product component template

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, input, Input, Output } from '@angular/core';
+import { Product } from '../../../shared/models/product.mode';
 
 @Component({
   selector: 'app-product',
@@ -10,9 +11,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class ProductComponent {
   // img = `https://picsum.photos/640/640?r=${Math.random()}`;
   // Para tener inputs, es decir, propiedades que se pueden pasar desde el padre al hijo se hace con el decorador @Input()
-  @Input({required: true}) img: string = ''; // Se inicializa con un valor por defecto y se pide que sea requerido
-  @Input({required: true}) title: string = '';
-  @Input({required: true}) price: number = 0;
+  // @Input({required: true}) img: string = ''; // Se inicializa con un valor por defecto y se pide que sea requerido
+  // @Input({required: true}) title: string = '';
+  // @Input({required: true}) price: number = 0;
+
+  @Input({required: true}) product!: Product;
 
   // se debe crear un evento para que el hijo pueda comunicarse con el padre
   @Output() addToCart = new EventEmitter();
@@ -21,6 +24,6 @@ export class ProductComponent {
   addToCartHandler() {
     console.log('Estamos en el hijo');
 
-    this.addToCart.emit(`Hola, este es un mensaje desde el producto: ${this.title}`);
+    this.addToCart.emit(`Hola, este es un mensaje desde el producto: ${this.product.title}`);
   }
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
+import { Product } from '../../../shared/models/product.mode';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Producto 1',
+        price: 100,
+        image: 'https://picsum.photos/640/480?r=1',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 2',
+        price: 200,
+        image: 'https://picsum.photos/640/480?r=12',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 3',
+        price: 300,
+        image: 'https://picsum.photos/640/480?r=123',
+        createdAt: new Date().toISOString()
+      }
+    ];
+
+    this.products.set(initProducts);
+  }
+
   // desde el padre se crea un metodo que recibe un evento del hijo
   fromChild(event: string) {
     console.log('Estamos en el padre:', event);
