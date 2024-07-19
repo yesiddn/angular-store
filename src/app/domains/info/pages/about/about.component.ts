40af4bd8ed85c02a77cf9c0603acfbfef6b0e7af
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-07-19 13:48:10 -0500

    chore: Update about component template and imports

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { CounterComponent } from '../../../shared/components/counter/counter.component';
 import { CommonModule } from '@angular/common';
+import { WaveAudioComponent } from '../../components/wave-audio/wave-audio.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule, CounterComponent],
+  imports: [CommonModule, CounterComponent, WaveAudioComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
