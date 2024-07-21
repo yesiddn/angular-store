801d0192f59c1e8e72f2bf682e04dea9ac8560c7
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-07-21 17:16:38 -0500

    chore: Add short imports in TypeScript

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,10 +1,10 @@
 import { Component, inject, signal } from '@angular/core';
-import { ProductComponent } from '../../components/product/product.component';
-import { Product } from '../../../shared/models/product.mode';
+import { ProductComponent } from '@products/components/product/product.component';
+import { Product } from '@shared/models/product.mode';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from '../../../shared/components/header/header.component';
-import { CartService } from '../../../shared/services/cart.service';
-import { ProductService } from '../../../shared/services/product.service';
+import { HeaderComponent } from '@shared/components/header/header.component';
+import { CartService } from '@shared/services/cart.service';
+import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-list',
