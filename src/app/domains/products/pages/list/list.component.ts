57ecfc49b7cb504461a0c4601c7d0eca4309f3b2
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-07-21 17:09:01 -0500

    chore: Add use of HTTP request

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -4,6 +4,7 @@ import { Product } from '../../../shared/models/product.mode';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { CartService } from '../../../shared/services/cart.service';
+import { ProductService } from '../../../shared/services/product.service';
 
 @Component({
   selector: 'app-list',
@@ -16,39 +17,52 @@ export class ListComponent {
   products = signal<Product[]>([]);
   // cart = signal<Product[]>([]);
   private cartService = inject(CartService);
+  private productService = inject(ProductService);
 
-  constructor() {
-    const initProducts: Product[] = [
-      {
-        id: Date.now(),
-        title: 'Producto 1',
-        price: 100,
-        image: 'https://picsum.photos/640/480?r=1',
-        createdAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'Producto 2',
-        price: 200,
-        image: 'https://picsum.photos/640/480?r=12',
-        createdAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'Producto 3',
-        price: 300,
-        image: 'https://picsum.photos/640/480?r=123',
-        createdAt: new Date().toISOString()
-      }
-    ];
+  // constructor() {
+  //   const initProducts: Product[] = [
+  //     {
+  //       id: Date.now(),
+  //       title: 'Producto 1',
+  //       price: 100,
+  //       image: 'https://picsum.photos/640/480?r=1',
+  //       createdAt: new Date().toISOString()
+  //     },
+  //     {
+  //       id: Date.now(),
+  //       title: 'Producto 2',
+  //       price: 200,
+  //       image: 'https://picsum.photos/640/480?r=12',
+  //       createdAt: new Date().toISOString()
+  //     },
+  //     {
+  //       id: Date.now(),
+  //       title: 'Producto 3',
+  //       price: 300,
+  //       image: 'https://picsum.photos/640/480?r=123',
+  //       createdAt: new Date().toISOString()
+  //     }
+  //   ];
 
-    this.products.set(initProducts);
-  }
+  //   this.products.set(initProducts);
+  // }
 
   // desde el padre se crea un metodo que recibe un evento del hijo
   // fromChild(event: string) {
   //   console.log('Estamos en el padre:', event);
   // }
+
+  ngOnInit() {
+    this.productService.getProducts()
+      .subscribe({
+        next: (products) => {
+          this.products.set(products);
+        },
+        error: (error) => {
+          console.error('Error:', error);
+        }
+      });
+  }
 
   addToCart(product: Product) {
     // this.cart.update(prevState => [...prevState, product]);
